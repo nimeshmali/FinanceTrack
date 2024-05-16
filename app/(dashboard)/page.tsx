3019f38a6589990d7d246ba3cd541c417699222b
unknown <malinimesh03@gmail.com>
2024-05-16 15:36:41 +0530

    Auth setup done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button";
+
+import { UserButton } from "@clerk/nextjs";
 // import React, { Component } from 'react'
 
 export default function Home() {
   return (
-    <Button>
-      hellos
-    </Button>
+    <UserButton afterSignOutUrl="/"/>
   );
 }
