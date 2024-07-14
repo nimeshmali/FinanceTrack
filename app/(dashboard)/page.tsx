04e1f981b0403087b09632d75dccd6b3732e12a2
unknown <malinimesh03@gmail.com>
2024-07-14 19:06:56 +0530

    chart done

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,10 +1,11 @@
-"use client";
-import { UserButton } from "@clerk/nextjs";
+import { DataCharts } from "@/components/data-charts";
+import { DataGrid } from "@/components/data-grid";
 
-import { useGetAccounts } from "@/features/accounts/api/use-get-accounts";
-import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return <div></div>;
+export default function DashboardPage() {
+  return (
+    <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
+      <DataGrid />
+      <DataCharts />
+    </div>
+  );
 }
